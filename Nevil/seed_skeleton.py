a89a3366168174eabcb32acf29d885a5e170ec66
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-09-10 17:07:55 +0530

    skeleton for decryption added

--- Nevil/seed_skeleton.py
+++ Nevil/seed_skeleton.py
@@ -5,7 +5,79 @@ Created on Sat Sep 10 16:12:42 2016
 @author: Nevil Dsouza
 """
 
-### SKELETON FOR SEED ENCRYPTION PROGRAM
+#### SKELETON FOR SEED ENCRYPTION PROGRAM
+#
+#def K(i):
+#    print("inside K",i)
+#    return i
+#
+#def F(k,r):
+#    print("inside F",k,r)
+#    return r
+#
+#def seed_encrypt(pt):
+#    print("inside seed_encrypt with PT=",pt)
+#        
+#    # int to binary
+#    bpt = "{0:b}".format(pt)
+#    print("binary is ",bpt)
+#    
+#    # binary to int
+#    pt = int(bpt,2)
+#    print("integer is",pt)
+#    
+#    #make bpt 128 bit long
+#    diff = 128 - len(bpt)
+#    zeros = '0'*diff
+#    rev_bpt=bpt[::-1]
+#    temp = rev_bpt+zeros
+#    bpt = temp[::-1]
+#    
+#    #print(rev_bpt,"\n",temp,"\n",bpt,"\n")
+#    
+#    #test 128 bit pt
+#    #print(int(bpt,2))
+#    #successful
+#    
+#    # divide bpt into L and R
+#    l=bpt[0:64]
+#    r=bpt[64:127]
+#    #print("-1",l,r)
+#    
+#    # 15 rounds
+#    for i in range(15):
+#        t=r
+#        r="{0:b}".format(int(l,2)^int(F(K(i),r),2))
+#        l=t
+#        #print(i,l,r)
+#    
+#    #last round
+#    l="{0:b}".format(int(l,2)^int(F(K(i),r),2))
+#    r=r
+#    #print("15",l,r)
+#    
+#    bpt=l+r
+#    
+#    
+#    return int(bpt,2)
+#
+#
+#
+#### MAIN ENTRY FOR ENCRYPTION
+#
+#PT="45"
+#
+#CT=seed_encrypt(int(PT))
+#
+#print("Encrypted CT=",CT)
+#
+#### END
+
+
+
+
+
+#### SKELETON FOR SEED DECRYPTION PROGRAM
 
 def K(i):
     print("inside K",i)
@@ -15,37 +87,37 @@ def F(k,r):
     print("inside F",k,r)
     return r
 
-def seed_encrypt(pt):
-    print("inside seed_encrypt with PT=",pt)
+def seed_decrypt(ct):
+    print("inside seed_decrypt with CT=",ct)
         
     # int to binary
-    bpt = "{0:b}".format(pt)
-    print("binary is ",bpt)
+    bct = "{0:b}".format(ct)
+    print("binary is ",bct)
     
     # binary to int
-    pt = int(bpt,2)
-    print("integer is",pt)
+    ct = int(bct,2)
+    print("integer is",ct)
     
-    #make bpt 128 bit long
-    diff = 128 - len(bpt)
+    #make bct 128 bit long
+    diff = 128 - len(bct)
     zeros = '0'*diff
-    rev_bpt=bpt[::-1]
-    temp = rev_bpt+zeros
-    bpt = temp[::-1]
+    rev_bct=bct[::-1]
+    temp = rev_bct+zeros
+    bct = temp[::-1]
     
-    #print(rev_bpt,"\n",temp,"\n",bpt,"\n")
+    #print(rev_bct,"\n",temp,"\n",bct,"\n")
     
-    #test 128 bit pt
-    #print(int(bpt,2))
+    #test 128 bit ct
+    #print(int(bct,2))
     #successful
     
-    # divide bpt into L and R
-    l=bpt[0:64]
-    r=bpt[64:127]
+    # divide bct into L and R
+    l=bct[0:64]
+    r=bct[64:127]
     #print("-1",l,r)
     
     # 15 rounds
-    for i in range(15):
+    for i in range(14,0,-1):
         t=r
         r="{0:b}".format(int(l,2)^int(F(K(i),r),2))
         l=t
@@ -56,23 +128,20 @@ def seed_encrypt(pt):
     r=r
     #print("15",l,r)
     
-    bpt=l+r
+    bct=l+r
     
     
-    return int(bpt,2)
+    return int(bct,2)
 
 
 
-### MAIN ENTRY FOR ENCRYPTION
+### MAIN ENTRY FOR DECRYPTION
 
-PT="45"
+CT="726"
 
-CT=seed_encrypt(int(PT))
+PT=seed_decrypt(int(CT))
 
-print("Encrypted CT=",CT)
+print("Decrypted PT=",PT)
 
 ### END
-
-
-
 
