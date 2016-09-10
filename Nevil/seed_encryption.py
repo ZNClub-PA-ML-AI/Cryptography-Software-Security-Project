4da8d09c41fd5223479d99b028a1cb7e2464bd0d
Nevil Dsouza <dsouza.nevil45@gmail.com>
2016-09-10 16:15:56 +0530

    1st layer skeleton done

---
+++ Nevil/seed_encryption.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 16:10:39 2016
+
+@author: Nevil Dsouza
+"""
+
+### SKELETON FOR SEED ENCRYPTION PROGRAM
+### 
+### 
+
+
+
+
+
+### main entry
---
+++ Nevil/seed_skeleton.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 16:12:42 2016
+
+@author: Nevil Dsouza
+"""
+
+### SKELETON FOR SEED ENCRYPTION PROGRAM
+
+def seed_encrypt(pt):
+    print("inside seed_encrypt")
+    return
+
+
+
+### MAIN ENTRY FOR ENCRYPTION
+
+PT=""
+
+CT=seed_encrypt(pt)
+
+
+
+
+
